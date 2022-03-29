294acfda2db592592f82a8c4c2bf39a148efb703
Kal <kal.aalrajhi@gmail.com>
2022-03-28 21:10:39 -0600

    Outline Recipe Repository Test

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -2,6 +2,12 @@ import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 
 describe('Recipe', () => {
+    let recipeRepository;
+
+  beforeEach(() => {
+    recipeRepository = new RecipeRepository();
+  });
+
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
