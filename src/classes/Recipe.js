9e03ce750bbe2342f8e504b5fdf8d95c91f91092
Kal <kal.aalrajhi@gmail.com>
2022-03-30 12:56:32 -0600

    Add Recipe methods

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -4,40 +4,103 @@ export class Recipe {
     constructor(recipe) {
         this.id = recipe.id;
         this.image = recipe.image;
-        this.ingredients = recipe.ingredients; 
+        this.recipeIngredients = recipe.ingredients; 
         this.instructions = recipe.instructions;
         this.name = recipe.name;
         this.tags = recipe.tags;
     }
 
-    // getIngredientDetails(ingredientsDictionary) {
-    //     let ingredientDetails = this.ingredients.map((ingredientFromRecipe) => {
-    //             let idMatch = ingredientsDictionary.find(ingredientFromDictionary => ingredientFromDictionary.id === ingredientFromRecipe.id)
-    //             let currentIngredient = new Ingredient(idMatch);
-    //             return currentIngredient;
-    //         });
-    //         return ingredientDetails;
-    // }
-
-    getIngredientNames(ingredientsDictionary) {
-        let ingredientNames = this.ingredients.map((ingredientFromRecipe) => {
-                let idMatch = ingredientsDictionary.find(ingredientItem => ingredientItem.id === ingredientFromRecipe.id)
+    getIngredientDetails(ingredientsData) {
+        let ingredientDetails = this.recipeIngredients.map((recipeIngredient) => {
+                let idMatch = ingredientsData.find(ingredientData => recipeIngredient.id === ingredientData.id);
                 let currentIngredient = new Ingredient(idMatch);
-                return currentIngredient.name;
+                return currentIngredient;
             });
-            console.log(ingredientNames);
+            return ingredientDetails;
     }
-        
-    calculateCost(ingredient) { // takes in a single ingredient
-        // calculate the cost of the ingredient by cost in cents * quantity
+
+    getIngredientNames(ingredientsData) {
+        let ingredientDetails = this.getIngredientDetails(ingredientsData);
+        let ingredientNames = ingredientDetails.map(ingredientDetail => ingredientDetail.name);
+        return ingredientNames;
     }
     
-    getTotalCost(ingredients) {// takes in an array of ingredients
-        // runs through the array of ingredients summing up the total cost.
-        // invokes `calculcateCost()`
+    getTotalCost(ingredientsData) {
+        let ingredientDetails =  this.getIngredientDetails(ingredientsData);
+        let totalCostInCents = ingredientDetails.reduce((total, ingredientDetail, idx) => {
+        return total + (ingredientDetail.estimatedCostInCents * this.recipeIngredients[idx].quantity.amount);
+        }, 0);
+        console.log(totalCostInCents);
+        var totalCostInDollars = (totalCostInCents / 100).toFixed(2); // conver to dollars
+        return totalCostInDollars;
     }
     
     getInstructions() {
-        return 
+        return this.instructions;
     }
-    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+//     import { Ingredient } from './Ingredient';
+
+// export class Recipe {
+//     constructor(recipe) {
+//         this.id = recipe.id;
+//         this.image = recipe.image;
+//         this.ingredients = recipe.ingredients; 
+//         this.instructions = recipe.instructions;
+//         this.name = recipe.name;
+//         this.tags = recipe.tags;
+//     }
+
+//     // getIngredientDetails(ingredientsDictionary) {
+//     //     let ingredientDetails = this.ingredients.map((ingredientFromRecipe) => {
+//     //             let idMatch = ingredientsDictionary.find(ingredientFromDictionary => ingredientFromDictionary.id === ingredientFromRecipe.id)
+//     //             let currentIngredient = new Ingredient(idMatch);
+//     //             return currentIngredient;
+//     //         });
+//     //         return ingredientDetails;
+//     // }
+
+//     getIngredientNames(ingredientsDictionary) {
+//         let ingredientNames = this.ingredients.map((ingredientFromRecipe) => {
+//                 let idMatch = ingredientsDictionary.find(ingredientFromDictionary => ingredientFromDictionary.id === ingredientFromRecipe.id)
+//                 let currentIngredient = new Ingredient(idMatch);
+//                 return currentIngredient.name;
+//             });
+//             console.log(ingredientNames);
+//     }
+    
+//     getTotalCost(ingredientsDictionary) {// takes in an array of ingredients
+//         let ingredientPricesInCents = this.ingredients.map((ingredientFromRecipe) => {
+//             let idMatch = ingredientsDictionary.find(ingredientFromDictionary => ingredientFromDictionary.id === ingredientFromRecipe.id)
+//             let currentIngredient = new Ingredient(idMatch);
+//             return currentIngredient.estimatedCostInCents * ingredientFromRecipe.quantity.amount;
+//         });
+//         console.log(ingredientPricesInCents);
+
+//         let totalCostInCents = ingredientPricesInCents.reduce((acc, price) => {
+//             return acc + price;
+//         }, 0);
+
+//         var totalCostInDollars = (totalCostInCents / 100).toFixed(2); // conver to dollars
+
+//         console.log(totalCostInCents);
+//         console.log(totalCostInDollars);
+
+//     }
+    
+//     getInstructions() {
+//         return this.instructions;
+//     }
+//     };
--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -4,14 +4,13 @@ import { recipesSampleData } from '../src/data/recipes-sample-data';
 import { ingredientsData } from '../src/data/ingredients';
     
 describe('Recipe', () => {
+    let allIngredientsData;
     let recipe;
     let recipeData;
-    let ingredientDictionary;
-
     beforeEach(() => {
+        allIngredientsData = ingredientsData;
         recipeData = recipesSampleData[0];
         recipe = new Recipe(recipeData);
-        ingredientDictionary = ingredientsData;
     });
 
     it('should create a new instance of Recipe', () => {
@@ -27,92 +26,12 @@ describe('Recipe', () => {
     });
 
     it('should have ingredients', () => {
-        expect(recipe.ingredients).to.deep.equal(
-            [
-                {
-                    "id": 20081,
-                    "quantity": {
-                    "amount": 1.5,
-                    "unit": "c"
-                    }
-                },
-                {
-                    "id": 18372,
-                    "quantity": {
-                    "amount": 0.5,
-                    "unit": "tsp"
-                    }
-                },
-                {
-                    "id": 1123,
-                    "quantity": {
-                    "amount": 1,
-                    "unit": "large"
-                    }
-                },
-                {
-                    "id": 19335,
-                    "quantity": {
-                    "amount": 0.5,
-                    "unit": "c"
-                    }
-                },
-                {
-                    "id": 19206,
-                    "quantity": {
-                    "amount": 3,
-                    "unit": "Tbsp"
-                    }
-                },
-                {
-                    "id": 19334,
-                    "quantity": {
-                    "amount": 0.5,
-                    "unit": "c"
-                    }
-                },
-                {
-                    "id": 2047,
-                    "quantity": {
-                    "amount": 0.5,
-                    "unit": "tsp"
-                    }
-                },
-                {
-                    "id": 1012047,
-                    "quantity": {
-                    "amount": 24,
-                    "unit": "servings"
-                    }
-                },
-                {
-                    "id": 10019903,
-                    "quantity": {
-                    "amount": 2,
-                    "unit": "c"
-                    }
-                },
-                {
-                    "id": 1145,
-                    "quantity": {
-                    "amount": 0.5,
-                    "unit": "c"
-                    }
-                },
-                {
-                    "id": 2050,
-                    "quantity": {
-                    "amount": 0.5,
-                    "unit": "tsp"
-                    }
-                }
-                ]
-        );
+        expect(recipe.recipeIngredients).to.equal(recipeData.ingredients); 
     });
 
     it.only('should get ingredient names', () => {
-        recipe.getIngredientNames(ingredientDictionary);
-        // expect(recipe.getIngredientNames()).to.equal('https://spoonacular.com/recipeImages/595736-556x370.jpg');
+        //  recipe.getIngredientDetails(allIngredientsData);
+        // recipe.getIngredientNames(allIngredientsData);
+        recipe.getTotalCost(allIngredientsData);
     });
-
 });
