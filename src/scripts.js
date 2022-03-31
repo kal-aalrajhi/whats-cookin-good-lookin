731203fcb9356c3b961e7a3a83d73dbfd5f088a9
Kyle Howard <kylehoward1228@yahoo.com>
2022-03-31 13:51:21 -0600

    Add js to start and slight html refactor

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,18 @@ import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+let showAllRecipesButton = document.querySelector('.nav-btn');
+let homeView = document.querySelector('.home-view')
 
+showAllRecipes.addEventListener("click", showAll() )
+
+let show => (element) {
+  element.classList.remove("hidden")
+
+}
+
+var hide => (element) {
+  element.classList.add("hidden")
+}
 
 console.log('Hello world');
