ab204fe7513db84160412e0cc9ae468883aa7152
Lourdes Benites <lourdesben95@gmail.com>
2022-03-31 20:34:23 -0600

    Import data and instantiate recipe repository

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,20 @@
 import './styles.css';
 import apiCalls from './apiCalls';
+import { recipesSampleData } from '../src/data/recipes-sample-data';
+import { RecipeRepository } from '../src/classes/RecipeRepository';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+//Global Variables
+var allRecipeStorage = new RecipeRepository();
+var recipeData = recipesSampleData;
+// allRecipeStorage.addRecipe(recipeData[0]);
+// console.log(recipeData)
+// console.log(allRecipeStorage.recipes)
+
+console.log(recipeData[0])
+
+//Query Selectors
 var allRecipeView = document.querySelector("#allRecipeView");
 var homeView = document.querySelector("#homeView");
 var showAllRecipesButton = document.querySelector("#allRecipesBtn");
@@ -10,18 +22,18 @@ let findNameBtn = document.querySelector("#findNameBtn");
 let findTagBtn = document.querySelector("#findTagBtn");
 var homeBtn = document.querySelector("#homeBtn");
 
-
+//Event Listeners
 showAllRecipesButton.addEventListener("click", loadAllRecipesView);
 // findNameButton.addEventListener("click", );
 homeBtn.addEventListener("click", loadHomeView);
 // findTagButton.addEventListener("click", );
 
-
+//Functions
 function loadAllRecipesView() {
  hideAllViews();
  show(allRecipeView);
   allRecipeView.innerHTML = '';
-  for(var i = 0; i < 4; i++) {
+  for(var i = 0; i < recipeData.length; i++) {
 
     allRecipeView.innerHTML +=
     `<div class="box recipe-box">
