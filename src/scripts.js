d0d559ab82a01df866ab85b2b9ac2af99ab9aae2
Kyle Howard <kylehoward1228@yahoo.com>
2022-03-31 19:01:55 -0600

    Add basic innerHTML and refine buttons

--- src/scripts.js
+++ src/scripts.js
@@ -3,35 +3,50 @@ import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-
+var allRecipeView = document.querySelector("#allRecipeView");
 var homeView = document.querySelector("#homeView");
 var showAllRecipesButton = document.querySelector("#allRecipesBtn");
-let findNameButton = document.querySelector("#findNameBtn");
-let findTagButton = document.querySelector("#findTagBtn");
+let findNameBtn = document.querySelector("#findNameBtn");
+let findTagBtn = document.querySelector("#findTagBtn");
 var homeBtn = document.querySelector("#homeBtn");
 
 
-showAllRecipesButton.addEventListener("click", loadAllRecipesView)
-findNameButton.addEventListener("click", )
-homeBtn.addEventListener("click", homeView)
-findTagButton.addEventListener("click", )
+showAllRecipesButton.addEventListener("click", loadAllRecipesView);
+// findNameButton.addEventListener("click", );
+homeBtn.addEventListener("click", loadHomeView);
+// findTagButton.addEventListener("click", );
 
 
 function loadAllRecipesView() {
- hideElement(homeView)
- show(allRecipeView)
+ hideAllViews();
+ show(allRecipeView);
+  allRecipeView.innerHTML = '';
+  for(var i = 0; i < 4; i++) {
+
+    allRecipeView.innerHTML +=
+    `<div class="box recipe-box">
+     <p> Cupcake </p>
+    </div>`
+
+  }
 }
 
-function homeView() {
-  hideElement(allRecipeView)
-  hideElement(findByNameView)
-  hideElement(findByTagView)
+function loadHomeView() {
+  hideAllViews();
+  show(homeView);
 }
 
-var show => (element) {
-  element.classList.remove("hidden")
+function hideAllViews() {
+  hideElement(allRecipeView);
+  hideElement(findByNameView);
+  hideElement(findByTagView);
+  hideElement(homeView);
+}
+
+var show = (element) => {
+  element.classList.remove("hidden");
 }
 
 var hideElement = (element) => {
-  element.classList.add("hidden")
+  element.classList.add("hidden");
 }
