510ba3c7c29b7a7967d731dfaa707d27b1f542e1
Kal <kal.aalrajhi@gmail.com>
2022-04-01 08:35:27 -0600

    Add recipe card view

--- src/scripts.js
+++ src/scripts.js
@@ -1,45 +1,67 @@
 import './styles.css';
 import apiCalls from './apiCalls';
-import { recipesSampleData } from '../src/data/recipes-sample-data';
+import { recipeData } from '../src/data/recipes';
 import { RecipeRepository } from '../src/classes/RecipeRepository';
 import './images/turing-logo.png'; // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 
 // Global Variables
-var recipeData = recipesSampleData;
-var allRecipeStorage = new RecipeRepository(recipeData);
+var allRecipeData = recipeData;
+var allRecipeStorage = new RecipeRepository(allRecipeData);
 
 // Query Selectors
 var allRecipeView = document.querySelector("#allRecipeView");
 var homeView = document.querySelector("#homeView");
+var recipeDetailView = document.querySelector("#recipeDetailView")
 var showAllRecipesButton = document.querySelector("#allRecipesBtn");
+var homeBtn = document.querySelector("#homeBtn");
+var searchResultView = document.querySelector("#searchResultView"); 
 // var findNameBtn = document.querySelector("#findNameBtn");
 // var findTagBtn = document.querySelector("#findTagBtn");
-var homeBtn = document.querySelector("#homeBtn");
+
 
 // Event Listeners
 showAllRecipesButton.addEventListener("click", loadAllRecipesView);
-// findNameButton.addEventListener("click", );
 homeBtn.addEventListener("click", loadHomeView);
+searchResultView.addEventListener("click", loadRecipeDetailView);
+// findNameButton.addEventListener("click", );
 // findTagButton.addEventListener("click", );
 
 // Functions
+function loadHomeView() {
+  hideAllViews();
+  showElement(homeView);
+}
+
 function loadAllRecipesView() {
  hideAllViews();
- show(allRecipeView);
+ showElement(allRecipeView);
  allRecipeView.innerHTML = '';
 
  allRecipeStorage.recipes.forEach((recipe) => {
     allRecipeView.innerHTML += `
     <div class='box recipe-box'>
         <img id=${recipe.id} src=${recipe.image} alt='${recipe.name} image' />
-        <p class='recipe-name'>${recipe.name}</p>
+        <h4 class='recipe-name'>${recipe.name}</h4>
     </div>`
   });
 }
 
-function loadHomeView() {
-  hideAllViews();
-  show(homeView);
+function loadRecipeDetailView(event) {
+    if(event.target.id !== 'searchResultView') {
+        console.log("recipe ID is: ", event.target.id);
+        hideAllViews();
+        showElement(recipeDetailView);
+        recipeDetailView.innerHTML = `
+        <div class='recipe-card'>
+            <h3 class='recipe-name'>Recipe Name</h3>
+            <h4>Ingredients</h4>
+            <p>ingredients list</p>
+            <h4>Instructions</h4>
+            <p>instructions list</p>
+            <h4>Total Cost</h4>
+            <p>$cost</p>
+        </div>`;
+    }
 }
 
 function hideAllViews() {
@@ -47,9 +69,10 @@ function hideAllViews() {
   hideElement(findByNameView);
   hideElement(findByTagView);
   hideElement(homeView);
+  hideElement(recipeDetailView);
 }
 
-var show = (element) => {
+var showElement = (element) => {
   element.classList.remove("hidden");
 }
 
