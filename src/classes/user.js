c1b4b9691db75c04dae6fecceb2e9a7ab3c3261c
Kyle Howard <kylehoward1228@yahoo.com>
2022-04-02 10:11:30 -0600

    Add user class

---
+++ src/classes/user.js
@@ -0,0 +1,19 @@
+export class User {
+  constructor() {
+    this.favoriteRecipes = [];
+    this.recipesToCook = [];
+    this.star = false;
+  }
+  addFavoriteRecipe() {
+    favoriteRecipes.push()
+  }
+
+  addRecipesToCook() {
+    recipesToCook.push()
+  }
+    
+//need to filter favoriteRecipes by tag
+//need to filter favoriteRecipes by name
+//should recipes to cook be its own page/view?
+
+}
