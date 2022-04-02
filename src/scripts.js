e8e477e861e391e92456596219065160ceae7f9f
Kal <kal.aalrajhi@gmail.com>
2022-04-02 07:30:22 -0600

    Fix page view load clash

--- src/scripts.js
+++ src/scripts.js
@@ -12,77 +12,50 @@ var allRecipeStorage = new RecipeRepository();
 allRecipeStorage.addRecipes(allRecipeData);
 
 // Query Selectors
-var allRecipeView = document.querySelector("#allRecipeView");
 var homeView = document.querySelector("#homeView");
-var recipeDetailView = document.querySelector("#recipeDetailView")
-var showAllRecipesButton = document.querySelector("#allRecipesBtn");
-var homeBtn = document.querySelector("#homeBtn");
 var searchResultView = document.querySelector("#searchResultView");
+var allRecipeView = document.querySelector("#allRecipeView");
+var recipeDetailView = document.querySelector("#recipeDetailView")
+var findByNameView = document.querySelector("#findByNameView");
+var homeBtn = document.querySelector("#homeBtn");
+var showAllRecipesBtn = document.querySelector("#allRecipesBtn");
 var findNameBtn = document.querySelector("#findNameBtn");
 var findTagBtn = document.querySelector("#findTagBtn");
-var searchNameInput = document.querySelector("#searchByNameInput");
 var searchNameBtn = document.querySelector("#nameSearchBtn");
-var searchTagBtn = document.querySelector("#tagSearchBtn");
-var searchTagInput = document.querySelector("#searchByTagInput");
+var searchNameInput = document.querySelector("#searchByNameInput");
+var searchResultsView = document.querySelector("#searchResultsView");
+// var searchTagBtn = document.querySelector("#tagSearchBtn");
+// var searchTagInput = document.querySelector("#searchByTagInput");
 
 
 // Event Listeners
-showAllRecipesButton.addEventListener("click", loadAllRecipesView);
+showAllRecipesBtn.addEventListener("click", loadAllRecipesView);
 homeBtn.addEventListener("click", loadHomeView);
-searchResultView.addEventListener("click", loadRecipeDetailView);
+allRecipeView.addEventListener("click", loadRecipeDetailView);
+searchResultsView.addEventListener("click", loadRecipeDetailView);
 findNameBtn.addEventListener("click", loadNameSearchView);
-findTagBtn.addEventListener("click", loadTagSearchView);
 
 searchNameBtn.addEventListener("click", () => {
-  event.preventDefault();
   var searchingForName = grabSearchValue();
   searchRecipeByName(searchingForName);
 });
 
-searchTagBtn.addEventListener("click", () => {
-  event.preventDefault();
-  var searchingForTag = grabSearchValue();
-  searchRecipeByTag(searchingForTag);
-});
-
 // Functions
-function loadNameSearchView() {
-  hideAllViews();
-  showElement(findByNameView);
+function hideAllViews() {
+  hideElement(homeView);
+  hideElement(searchResultView);
+  hideElement(allRecipeView);
+  hideElement(findByNameView);
+  hideElement(recipeDetailView);
+  hideElement(searchResultsView);
 }
 
-function loadTagSearchView() {
-  hideAllViews();
-  showElement(findByTagView);
+var showElement = (element) => {
+  element.classList.remove("hidden");
 }
 
-function grabSearchValue() { //add conditional to determine if serach by name or by tag
-  return searchNameInput.value.toLowerCase();
-}
-
-function searchRecipeByName(searchingFor) {
-  var nameResult = allRecipeStorage.recipes.find((recipe) => {
-    return recipe.name.toLowerCase() === searchingFor
-  });
-
-  searchResultView.innerHTML = `
-  <div class='box recipe-box'>
-      <img id=${nameResult.id} src=${nameResult.image} alt='${nameResult.name} image' />
-      <h4 class='recipe-name'>${nameResult.name}</h4>
-  </div>`
-}
-
-function searchRecipeByTag(searchingFor) {
-  var tagResult = allRecipeStorage.recipes.find((recipe) => {
-    return recipe.name.toLowerCase() === searchingFor
-  });
-  console.log(tagResult);
-
-  searchResultView.innerHTML = `
-  <div class='box recipe-box'>
-      <img id=${nameResult.id} src=${nameResult.image} alt='${nameResult.name} image' />
-      <h4 class='recipe-name'>${nameResult.name}</h4>
-  </div>`
+var hideElement = (element) => {
+  element.classList.add("hidden");
 }
 
 function loadHomeView() {
@@ -90,8 +63,19 @@ function loadHomeView() {
   showElement(homeView);
 }
 
+function loadNameSearchView() {
+  hideAllViews();
+  showElement(searchResultView);
+  showElement(findByNameView);
+}
+
+function grabSearchValue() { //add conditional to determine if serach by name or by tag
+  return searchNameInput.value.toLowerCase();
+}
+
 function loadAllRecipesView() {
  hideAllViews();
+ showElement(searchResultView);
  showElement(allRecipeView);
  allRecipeView.innerHTML = '';
 
@@ -105,42 +89,42 @@ function loadAllRecipesView() {
 }
 
 function loadRecipeDetailView(event) {
-    if(event.target.id !== 'searchResultView' && event.target.id !== 'allRecipeView' && event.target.id !== 'recipeDetailView') {
-        hideAllViews();
-        showElement(recipeDetailView);
+  if(event.target.id !== 'searchResultView' && event.target.id !== 'allRecipeView' && event.target.id !== 'recipeDetailView') {
+    hideAllViews();
+    showElement(searchResultView);
+    showElement(recipeDetailView);
 
-        let currentRecipe = allRecipeStorage.filterById(event.target.id);
+    let currentRecipe = allRecipeStorage.filterById(event.target.id);
+    var instructionList = []
+    currentRecipe.instructions.forEach((instruction) => {
+      instructionList.push(instruction.instruction);
+    });
 
-        var instructionList = []
-        currentRecipe.instructions.forEach((instruction) => {
-          instructionList.push(instruction.instruction)
-
-        })
-        recipeDetailView.innerHTML = `
-        <div class='recipe-card'>
-            <h3 class='recipe-name'>${currentRecipe.name}</h3>
-            <h4>Ingredients</h4>
-            <p>${currentRecipe.getIngredientNames(allIngredientsData)}</p>
-            <h4>Instructions</h4>
-            <p>${instructionList}</p>
-            <h4>Total Cost</h4>
-            <p>$${currentRecipe.getTotalCostInDollars(allIngredientsData)}</p>
-        </div>`;
-    }
+    recipeDetailView.innerHTML = `
+    <div class='recipe-card'>
+        <h3 class='recipe-name'>${currentRecipe.name}</h3>
+        <h4>Ingredients</h4>
+        <p>${currentRecipe.getIngredientNames(allIngredientsData)}</p>
+        <h4>Instructions</h4>
+        <p>${instructionList}</p>
+        <h4>Total Cost</h4>
+        <p>$${currentRecipe.getTotalCostInDollars(allIngredientsData)}</p>
+    </div>`;
+  }
 }
 
-function hideAllViews() {
-  hideElement(allRecipeView);
-  hideElement(findByNameView);
-  hideElement(findByTagView);
-  hideElement(homeView);
-  hideElement(recipeDetailView);
-}
+function searchRecipeByName(searchingFor) {
+  event.preventDefault();
+  var nameResult = allRecipeStorage.recipes.find((recipe) => {
+    return recipe.name.toLowerCase() === searchingFor;
+  });
+  
+  showElement(searchResultsView);
+  searchResultsView.innerHTML = `
+  <div class='box recipe-box'>
+      <img id=${nameResult.id} src=${nameResult.image} alt='${nameResult.name} image' />
+      <h4 class='recipe-name'>${nameResult.name}</h4>
+  </div>`
 
-var showElement = (element) => {
-  element.classList.remove("hidden");
-}
-
-var hideElement = (element) => {
-  element.classList.add("hidden");
+  // loadRecipeDetailView();
 }
