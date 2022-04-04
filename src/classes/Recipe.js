36cbd0668d818483acf383ae2771d890d0d3e809
Kal <kal.aalrajhi@gmail.com>
2022-04-03 18:31:38 -0600

    Allow user to delete favorite recipes

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -8,6 +8,7 @@ export class Recipe {
         this.instructions = recipe.instructions;
         this.name = recipe.name;
         this.tags = recipe.tags;
+        this.favorite = false;
     }
 
     getIngredientDetails(ingredientsData) {
