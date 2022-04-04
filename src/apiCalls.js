cc61eca2d0f1a543093a3132e3edda2ee99d0f0e
Kyle Howard <kylehoward1228@yahoo.com>
2022-04-04 15:20:36 -0600

    Add semi-colons

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,11 @@
 export let allRecipeDataPromise = () => {
     return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes').then(response => response.json());
-}
+};
 
 export let allUsersPromise = () => {
     return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users').then(response => response.json());
-}
+};
 
 export let allIngredientsPromise = () => {
     return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients').then(response => response.json());
-}
+};
