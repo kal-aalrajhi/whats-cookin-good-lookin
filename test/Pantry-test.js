196bc4eaf0d0797afe41365d15664bb7d0e20956
Lourdes Benites <lourdesben95@gmail.com>
2022-04-12 20:30:01 -0600

    Add instance of new pantry test

---
+++ test/Pantry-test.js
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import { Ingredient } from '../src/classes/Ingredient';
+import { Pantry } from '../src/classes/Pantry';
+import { ingredientsSampleData } from '../src/data/ingredients-sample-data';
+import { usersSampleData } from '../src/data/users-sample-data.js';
+import { User } from '../src/classes/User.js'
+
+
+describe('Pantry', () => {
+    let pantry0;
+    let pantry1;
+    let pantryData;
+    let user1;
+
+    beforeEach(() => {
+        user1 = new User(usersSampleData[0]);
+        pantry0 = new Pantry();
+        pantry1 = new Pantry(user1.pantry);
+        
+    })
+
+    it('should create a new instance of Pantry', () => {
+        expect(pantry1).to.be.an.instanceof(Pantry);
+    })
+
+
+});
